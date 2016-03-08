15bb42397fec58f880232b7f1b4ff827f7196208
Kiril Tonev <kiril.tonev@gmail.com>
2016-03-08 17:25:29 +0100

    Add a test stub for RDF mapping test.

---
+++ src/test/java/eu/dareed/rdfmapper/RDFMappingTest.java
@@ -0,0 +1,21 @@
+package eu.dareed.rdfmapper;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import eu.dareed.eplus.model.idf.IDF;
+import eu.dareed.rdfmapper.xml.nodes.Mapping;
+import org.junit.BeforeClass;
+
+/**
+ * @author <a href="mailto:kiril.tonev@kit.edu">Kiril Tonev</a>
+ */
+public class RDFMappingTest {
+
+    private static Model model;
+    private static Mapping mapping;
+    private static IDF idf;
+
+    @BeforeClass
+    public void setUp() {
+
+    }
+}
