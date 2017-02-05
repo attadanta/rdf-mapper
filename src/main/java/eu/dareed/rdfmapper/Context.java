e52a520ea29b84680db7a153dc62837acd3def29
Kiril Tonev <kiril.tonev@gmail.com>
2017-02-05 16:32:53 +0100

    Scope contexts.

--- src/main/java/eu/dareed/rdfmapper/Context.java
+++ src/main/java/eu/dareed/rdfmapper/Context.java
@@ -6,16 +6,49 @@ import java.util.regex.Pattern;
 /**
  * @author <a href="mailto:kiril.tonev@kit.edu">Kiril Tonev</a>
  */
-public class Context {
+public class Context implements VariableResolver {
     public static final String indexVariable = "#";
     public static final String namedVariable = "$";
 
     private static final Pattern pattern = Pattern.compile(String.format("((\\%s)\\{(.+?)})|((%s)\\{(\\d+)?})", namedVariable, indexVariable));
 
+    private final Context broader;
     private final VariableResolver variableResolver;
+
+    public Context(Context broader, VariableResolver variableResolver) {
+        this.broader = broader;
+        this.variableResolver = variableResolver;
+    }
 
     public Context(VariableResolver variableResolver) {
         this.variableResolver = variableResolver;
+        this.broader = null;
+    }
+
+    public Context augment(VariableResolver resolver) {
+        return new Context(this, resolver);
+    }
+
+    @Override
+    public String resolveNamedVariable(String variableName) {
+        String currentContextResolution = variableResolver.resolveNamedVariable(variableName);
+
+        if (currentContextResolution == null) {
+            return broader != null ? broader.resolveNamedVariable(variableName) : null;
+        } else {
+            return currentContextResolution;
+        }
+    }
+
+    @Override
+    public String resolveIndex(int index) {
+        String currentContextResolution = variableResolver.resolveIndex(index);
+
+        if (currentContextResolution == null) {
+            return broader != null ? broader.resolveIndex(index) : null;
+        } else {
+            return currentContextResolution;
+        }
     }
 
     String resolveVariables(String sequence) {
@@ -31,9 +64,9 @@ public class Context {
                 String variableIndexCharacterSequence = matcher.group(6) == null ? "" : matcher.group(6);
 
                 if (variableName.isEmpty()) {
-                    result.append(variableResolver.resolveIndex(Integer.parseInt(variableIndexCharacterSequence)));
+                    result.append(resolveIndex(Integer.parseInt(variableIndexCharacterSequence)));
                 } else {
-                    result.append(variableResolver.resolveNamedVariable(variableName));
+                    result.append(resolveNamedVariable(variableName));
                 }
 
                 lastMatchIndex = matcher.end();
