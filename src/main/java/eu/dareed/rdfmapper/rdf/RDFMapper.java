5a90a58aee0e6305d9bedbe2376f8b44c805fbdf
Kiril Tonev <kiril.tonev@gmail.com>
2017-02-11 16:24:48 +0100

    Allow for multiple entity mappings.

--- src/main/java/eu/dareed/rdfmapper/rdf/RDFMapper.java
+++ src/main/java/eu/dareed/rdfmapper/rdf/RDFMapper.java
@@ -4,17 +4,16 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
-import com.thaiopensource.util.UriEncoder;
 import eu.dareed.rdfmapper.Environment;
 import eu.dareed.rdfmapper.MappingData;
 import eu.dareed.rdfmapper.MappingDataEntity;
 import eu.dareed.rdfmapper.NamespaceResolver;
 import eu.dareed.rdfmapper.xml.nodes.*;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +33,7 @@ public class RDFMapper {
         Environment environment = new Environment(resolver);
 
         List<Entity> classList = mapping.getEntities();
-        Map<String, Integer> classIndexMap = buildClassIndexMap(classList);
+        Map<String, List<Integer>> classIndexMap = buildClassIndexMap(classList);
 
         for (MappingDataEntity dataEntity : data.getDataEntities()) {
             Environment entityEnvironment = environment.augment(dataEntity);
@@ -44,31 +43,44 @@ public class RDFMapper {
                 log.info("Class `{}' not found in class map.", dataEntity.getType());
                 continue;
             }
-            Entity entityType = classList.get(classIndexMap.get(dataEntity.getType().toLowerCase()));
 
-            // add triples for 'is instance of class xy'
-            String subjectURL = entityEnvironment.resolveURL(entityType.getUri());
+            for (Integer entityIndex : classIndexMap.get(dataEntity.getType().toLowerCase())) {
+                Entity entityType = classList.get(entityIndex);
 
-            Resource subject = model.createResource(subjectURL);
-            for (String classURI : entityType.getTypes()) {
-                subject.addProperty(RDF.type, model.createResource(resolver.resolveURI(classURI)));
+                // add triples for 'is instance of class xy'
+                String subjectURL = entityEnvironment.resolveURL(entityType.getUri());
+
+                Resource subject = model.createResource(subjectURL);
+                for (String classURI : entityType.getTypes()) {
+                    subject.addProperty(RDF.type, model.createResource(resolver.resolveURI(classURI)));
+                }
+
+                // add triples for properties
+                for (Property classProperty : entityType.getProperties()) {
+                    model.add(classProperty.describe(subjectURL, entityEnvironment));
+                }
             }
 
-
-            // add triples for properties
-            for (Property classProperty : entityType.getProperties()) {
-                model.add(classProperty.describe(subjectURL, entityEnvironment));
-            }
         }
 
         return model;
     }
 
-    private Map<String, Integer> buildClassIndexMap(List<Entity> classList) {
-        Map<String, Integer> indexMap = new HashMap<>();
+    private Map<String, List<Integer>> buildClassIndexMap(List<Entity> classList) {
+        Map<String, List<Integer>> indexMap = new HashMap<>();
         for (int i = 0; i < classList.size(); i++) {
-            Entity clsEnt = classList.get(i);
-            indexMap.put(clsEnt.getName().toLowerCase(), i);
+            Entity entity = classList.get(i);
+            String classKey = entity.getName().toLowerCase();
+
+            List<Integer> entitiesList;
+            if (indexMap.containsKey(classKey)) {
+                entitiesList = indexMap.get(classKey);
+            } else {
+                entitiesList = new LinkedList<>();
+                indexMap.put(classKey, entitiesList);
+            }
+
+            entitiesList.add(i);
         }
         return indexMap;
     }
